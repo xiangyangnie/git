c7482d0fcde96f168fd6569be601f516567ce270
xiangyangnie <63287183+xiangyangnie@users.noreply.github.com>
2020-04-07 19:12:51 +0800

    Update HelloController.java
    添加了后缀

--- src/main/java/com/xiangyangnie/git/controller/HelloController.java
+++ src/main/java/com/xiangyangnie/git/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Git! Use SSH";
+        return "Hello Git!Use SSH,Github.";
     }
 }
